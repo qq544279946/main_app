56e8f065855b57c5a0a7700cb4671544cc53f548
shijingxian <544279946@qq.com>
2023-03-19 20:47:26 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import './App.css';
 import { registerMicroApps, start } from 'qiankun';
 import { useEffect } from 'react';
 
+const getAppConfig = async () => {
+  const isLocal = window.location.hostname === 'localhost';
+  if(isLocal){
+    const curApp = location.pathname.slice('/')?.[0]??'';
+    return await (await fetch(`//${window.location.host}/${curApp}/app_config.json`)).json();
+  }
+  return await (await fetch('//static.ppspace.top/app_config.json')).json();
+}
+
 function App() {
 
 useEffect(() => {
   const initApp = async () => {
-    const config = await (await fetch('//static.ppspace.top/app_config.json')).json();
+    const config = getAppConfig();
     registerMicroApps(config);
     start();
   }
