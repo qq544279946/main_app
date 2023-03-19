476d6a50ef33c7f8c3598974a38749859c7bc283
shijingxian <544279946@qq.com>
2023-03-19 18:43:15 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { registerMicroApps, start } from 'qiankun';
+import { useEffect } from 'react';
 
 function App() {
+
+useEffect(() => {
+  const initApp = async () => {
+    const config = await (await fetch('//static.ppspace.top/app_config.json')).json();
+    registerMicroApps(config);
+    start();
+  }
+
+  initApp();
+}, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app-container">
     </div>
   );
 }
