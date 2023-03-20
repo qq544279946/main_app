76296a0d3780e847b79dc8bf6d7daf318166b95b
shijingxian <544279946@qq.com>
2023-03-21 00:32:05 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import { useEffect } from 'react';
 
 const getAppConfig = async () => {
   const isLocal = window.location.hostname === 'localhost';
-  if(isLocal){
-    const curApp = window.location.pathname.slice('/')?.[0]??'';
+  if (isLocal) {
+    const curApp = window.location.pathname.slice('/')?.[0] ?? '';
     return await (await fetch(`//${window.location.host}/${curApp}/app_config.json`)).json();
   }
-  return await (await fetch('//static.ppspace.top/app_config.json')).json();
+  const res =  await (await fetch('https://static.ppspace.top/app_config.json')).json();
+  console.log(res);
+  return res;
 }
 
 function App() {
 
-useEffect(() => {
-  const initApp = async () => {
-    const config = getAppConfig();
-    registerMicroApps(config);
-    start();
-  }
+  useEffect(() => {
+    const initApp = async () => {
+      const config = await getAppConfig();
+      registerMicroApps(config);
+      start();
+    }
 
-  initApp();
-}, []);
+    initApp();
+  }, []);
   return (
     <div id="app-container">
     </div>
